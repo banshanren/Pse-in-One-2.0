d81f8d55535c4d9ebd271d3ebd7537202c2590d7
jiahai <835438564@qq.com>
2017-03-30 17:32:14 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python pse.py temp/data_pos.fasta temp/pse_pos_svm.txt DNA PseDNC -lamada 3 -w 0.05 -f svm -l +1
-python pse.py temp/data_neg.fasta temp/pse_neg_svm.txt DNA PseDNC -lamada 3 -w 0.05 -f svm -l -1
-python feature_combine.py temp/pse_pos_svm.txt temp/
-# python train.py temp/pos_svm.txt temp/neg_svm.txt -m dna.model -c 1 9 2 -g -3 1 2 -v 10
+# python pse.py temp/data_pos.fasta temp/pse_pos_svm.txt DNA PseDNC -lamada 3 -w 0.05 -f svm -l +1
+# python pse.py temp/data_neg.fasta temp/pse_neg_svm.txt DNA PseDNC -lamada 3 -w 0.05 -f svm -l -1
+# python nac.py temp/data_pos.fasta temp/nac_pos_svm.txt DNA Mismatch -k 3 -m 1 -f svm -l +1
+# python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA Mismatch -k 3 -m 1 -f svm -l +1
+# python feature_combine.py temp/pse_pos_svm.txt temp/
+python train.py temp/nac_pos_svm.txt temp/nac_neg_svm.txt -m dna.model -c 1 9 2 -g -3 1 2 -v 10
