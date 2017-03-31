62d31afec4d5a71cb6edd355175e8e70f5941cd7
jiahai <835438564@qq.com>
2017-03-31 16:32:02 +0800

    modify run.sh,add sbusequence

--- run.sh
+++ run.sh
@@ -16,13 +16,15 @@
 # python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA IDKmer -k 3 -m 1 -f svm -l -1
 
 # mismatch
-# python nac.py temp/data_pos.fasta temp/nac_pos_svm.txt DNA mismatch -k 2 -m 0 -f svm -l +1
-# python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA mismatch -k 2 -m 0 -f svm -l -1
+python nac.py temp/data_pos.fasta temp/nac_pos_svm.txt DNA mismatch -k 2 -m 0 -f svm -l +1
+python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA mismatch -k 2 -m 0 -f svm -l -1
+svm-scale -l 0 -u 1 temp/nac_pos_svm.txt > temp/nac_pos_svm_scaled.txt
+svm-scale -l 0 -u 1 temp/nac_neg_svm.txt > temp/nac_neg_svm_scaled.txt
 
-python nac.py temp/data_pos.fasta temp/nac_pos_svm.txt DNA subsequence -k 2 -delta 0.5 -f svm -l +1
-python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA subsequence -k 2 -delta 0.5 -f svm -l -1
+# python nac.py temp/data_pos.fasta temp/nac_pos_svm.txt DNA subsequence -k 2 -delta 0.5 -f svm -l +1
+# python nac.py temp/data_neg.fasta temp/nac_neg_svm.txt DNA subsequence -k 2 -delta 0.5 -f svm -l -1
 
 # python feature_combine.py temp/pse_pos_svm.txt temp/
 
+# python train.py temp/nac_pos_svm_scaled.txt temp/nac_neg_svm_scaled.txt -m dna.model -c 1 9 2 -g -3 1 2 -v 10
 python train.py temp/nac_pos_svm.txt temp/nac_neg_svm.txt -m dna.model -c 1 9 2 -g -3 1 2 -v 10
- 
